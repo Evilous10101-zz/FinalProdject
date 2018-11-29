c46ec9573a69f807bb619cd48b701aee295e6dbb
Michael Lannon <sMichaelLannon@yccc.edu>
2018-11-28 19:00:07 -0500

    Can find the file but cannot upload due to error in dropbox api

--- src/dropbox/Upload.java
+++ src/dropbox/Upload.java
@@ -13,32 +13,21 @@ import java.io.InputStream;
 import java.io.IOException;
 
 public class Upload {
-    private static final String ACCESS_TOKEN = "-rlCNtKZ95AAAAAAAAAACZfSpYr5KbrRxC3KHZXAKjUJIWI2ZxHEhaAZxGLf2wpj";
+    private static final String ACCESSTOKEN = "-rlCNtKZ95AAAAAAAAAACZfSpYr5KbrRxC3KHZXAKjUJIWI2ZxHEhaAZxGLf2wpj";
 
-    public static void upload() throws DbxException, IOException {
+    @SuppressWarnings({ "deprecation", "unused" })
+	public static void upload() throws DbxException, IOException {
         // Create Dropbox client
         DbxRequestConfig config = new DbxRequestConfig("dropbox/java-tutorial", "en_US");
-        DbxClientV2 client = new DbxClientV2(config, ACCESS_TOKEN);
+        DbxClientV2 client = new DbxClientV2(config, ACCESSTOKEN);
 
         // Get current account info
         FullAccount account = client.users().getCurrentAccount();
         System.out.println("Owner of dropbox account: " + account.getName().getDisplayName());
 
-        // Get files and folder metadata from Dropbox root directory
-        ListFolderResult result = client.files().listFolder("");
-        while (true) {
-            for (Metadata metadata : result.getEntries()) {
-                System.out.println(metadata.getPathLower());
-            }
+        
 
-            if (!result.getHasMore()) {
-                break;
-            }
-
-            result = client.files().listFolderContinue(result.getCursor());
-        }
-
-        // Upload "test.txt" to Dropbox
+        // Uploads File to Dropbox
         try (InputStream in = new FileInputStream("C:\\Programming Class\\Eclipse\\FinalProdject\\File")) {
             FileMetadata metadata = client.files().uploadBuilder("C:\\Programming Class\\Eclipse\\FinalProdject\\File")
                 .uploadAndFinish(in);
